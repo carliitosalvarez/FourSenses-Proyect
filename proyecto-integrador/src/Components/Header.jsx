3634bae036fe428e7fa347f162e006554044b4a1
BrahianPuschiasis <brahian.puschiasis@gmail.com>
2023-10-25 03:55:42 -0300

    el header linkea a home

--- proyecto-integrador/src/Components/Header.jsx
+++ proyecto-integrador/src/Components/Header.jsx
@@ -1,39 +1,21 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 
-
-
-
-const Header = () =>{
-    
-
+const Header = () => {
     return (
         <Navbar className="nav">
+            <Link to="/">
                 <img src="../images/FourSenses-Logo.png" alt=""/>
-                <div className="buttons">
+            </Link>
+            <div className="buttons">
                 <Button className="bt-nav" variant="danger" type="submit">Iniciar Sesion</Button>{" "}
                 <Button className="bt-nav" variant="danger" type="submit">Crear Cuenta</Button>{" "}
-                </div>
+            </div>
         </Navbar>
-      );
-
-
-    // return(
-    // <header>
-    //     <Link to="">
-    //     <img src="./assets/FourSenses-Logo.png" alt="logo"/>
-    //     <p>lema de la empresa</p>
-    //     </Link>
-    //     <nav>
-    //         <ul>
-    //             <li><Link to="">"crear cuenta"</Link></li>
-    //             <li><Link to="">"iniciar sesion"</Link></li>
-    //         </ul>
-    //     </nav>
-    // </header>
-    // );
+    );
 }
 
 export default Header;
