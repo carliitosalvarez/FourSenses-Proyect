8a38d5dcd09ad407ca89a107e979242d2a0d8334
BrahianPuschiasis <brahian.puschiasis@gmail.com>
2023-10-25 01:18:34 -0300

    funciona la alta

--- proyecto-integrador/src/App.jsx
+++ proyecto-integrador/src/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import { useState } from 'react'
 import {Route, Routes} from "react-router-dom"
 import './App.css'
@@ -6,6 +8,7 @@ import Header from './components/Header'
 import Menu from "./Routes/Home"
 import Home from "./Routes/Home"
 import Product from "./Routes/Product"
+import Alta from './Routes/Alta'
 function App() {
 
   return (
@@ -15,6 +18,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/menu' element={<Menu/>}/>
         <Route path='/productos' element={<Product/>}/>
+        <Route path='/alta' element={<Alta/>}/>
         </Routes>
     </div>
 
