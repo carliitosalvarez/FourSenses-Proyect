64525c254672c3983dc370e5ad6c8bf6d883fadb
BrahianPuschiasis <brahian.puschiasis@gmail.com>
2023-10-26 01:21:42 -0300

    parece que termine

--- proyecto-integrador/src/Components/Footer.jsx
+++ proyecto-integrador/src/Components/Footer.jsx
@@ -2,6 +2,10 @@
 import React from "react";
 import "../Footer.css";
 import logo from "../assets/copyright.png"; // Ruta de tu imagen
+import imagen1 from "../assets/instagram.png"; // Ruta de la imagen 1
+import imagen2 from "../assets/email.png"; // Ruta de la imagen 2
+import imagen3 from "../assets/ubicacion.png"; // Ruta de la imagen 3
+import imagen4 from "../assets/telefono.png"; // Ruta de la imagen 4
 
 const Footer = () => {
   return (
@@ -9,6 +13,12 @@ const Footer = () => {
       <div className="left-content">
         <img src={logo} alt="Logo" className="logo" />
         <p>2023 FourSenses</p>
+      </div>
+      <div className="right-content">
+        <img src={imagen1} alt="Imagen 1" className="footer-img" />
+        <img src={imagen2} alt="Imagen 2" className="footer-img" />
+        <img src={imagen3} alt="Imagen 3" className="footer-img" />
+        <img src={imagen4} alt="Imagen 4" className="footer-img" />
       </div>
     </footer>
   );
